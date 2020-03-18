000b4f1facc1d14afa512b983c5cf0b9deb4d1db
Matheus Leite <matheus.leite.san@gmail.com>
2020-03-18 15:33:08 -0300

    create route test

--- tests/Route/RouterTest.php
+++ tests/Route/RouterTest.php
@@ -3,8 +3,23 @@
 namespace App\Test\Route;
 
 use App\Route\Router;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
 
-class RouterTest extends \PHPUnit_Framework_TestCase
+class RouterTest extends TestCase
 {
+    public function test_RouteDispatch_ShouldThrowInvalidArgumentException_WhenGivenAnUnregisteredRoute(): void
+    {
+        $unregisteredRoute = '/unregistered-route';
+        $notFoundCode      = 204;
 
+        $route = new Router();
+        $route->registry('/user', \App\User\UserController::class, 'index');
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("'{$unregisteredRoute}' is an unregistered route");
+        $this->expectExceptionCode($notFoundCode);
+
+        $route->dispatch($unregisteredRoute);
+    }
 }
