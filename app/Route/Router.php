8a926a8c3ddc2c14898eba4a05d838dce04488aa
Matheus Leite <matheus.leite.san@gmail.com>
2020-03-20 11:30:22 -0300

    Change default timezone

--- app/Route/Router.php
+++ app/Route/Router.php
@@ -17,13 +17,13 @@ final class Router implements RouterInterface
 
     public function dispatch(string $uri, array $params = []): void
     {
-        if (key_exists($uri, $this->routes)) {
-            $uriContent                = $this->routes[$uri];
-            $reflectedControllerMethod = self::createReflectionMethod($uriContent);
-            $reflectedControllerMethod->invokeArgs(new $uriContent['namespace'], $params);
+        if (!key_exists($uri, $this->routes)) {
+            throw new InvalidArgumentException("'{$uri}' is an unregistered route", StatusCodeEnum::NOT_FOUND());
         }
 
-        throw new InvalidArgumentException("'{$uri}' is an unregistered route", StatusCodeEnum::NOT_FOUND());
+        $uriContent                = $this->routes[$uri];
+        $reflectedControllerMethod = self::createReflectionMethod($uriContent);
+        $reflectedControllerMethod->invokeArgs(new $uriContent['namespace'], $params);
     }
 
     public function registry(string $uri, string $controller, string $method): void
--- bootstrap/index.php
+++ bootstrap/index.php
@@ -1,5 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../app/Route/index.php';
-
 require_once __DIR__ . '/../app/DotEnvLoader/index.php';
+
+require_once __DIR__ . '/../app/Date/index.php';
+
+require_once __DIR__ . '/../app/Route/index.php';
